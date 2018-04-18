4e9fbf3d2e940ed71428051a61b1b701d17d9b88
heppatytto99 <mihailma@metropolia.fi>
2018-04-18 17:52:57 +0300

    virus luokka

--- src/Virus.java
+++ src/Virus.java
@@ -12,6 +12,8 @@ public class Virus{
     Luo satunnaisen kokonaisluvun 'i' väliltä 0 - 100 ja palauttaa true
     jos i ei ole suurempi kuin tämän mahdollisuus tartuttaa.
     ESIM. Tämän mahdollisuus tartuttaa on 70% eli infectionChance on 70
+    randomilla on 70 lukua jotka ovat alle infectionChance ja 30 lukua
+    jotka ovat suuremmat kuin infectionChance eli 70/100 = 70% ja 30/100 = 30%
      */
     public boolean infect(){
         int i = ThreadLocalRandom.current().nextInt(0, 100);
