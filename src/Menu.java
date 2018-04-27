79f72956d13572d111a6f64c570837e86a2af02e
Petra <petra.silavuori@gmail.com>
2018-04-27 20:50:15 +0300

    Update Menu.java

--- src/Menu.java
+++ src/Menu.java
@@ -1,60 +1,71 @@
-import javafx.application.Application;
-import javafx.beans.property.SimpleBooleanProperty;
-import javafx.scene.Scene;
-import javafx.scene.layout.BorderPane;
-import javafx.stage.Stage;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.Slider;
+import javafx.scene.layout.VBox;
 
-public class Main extends Application{
+public class Menu {
 
-    private Stage stage;
-    private Simulation simulation;
-    private SimpleBooleanProperty ready = new SimpleBooleanProperty(true);
+    private static VBox vbox = new VBox(5);
+    private static Slider targetCount = new Slider(1.0, 1000000.0, 1.0);
+    private static Slider infectionChance = new Slider(1.0, 100.0, 1.0);
+    private static Button next = new Button("NEXT");
+    private static Button prev = new Button("PREVIOUS");
+    private static Button newSimulation = new Button("NEW SIMULATION");
+    private static Button closeSimulation = new Button("CLOSE SIMULATION");
 
-    public void start(Stage primaryStage) throws Exception{
-
-        stage = primaryStage;
-        stage.setTitle("VIRUS SIMULATION");
-        BorderPane border = new BorderPane();
-        Menu.createMenu();
-        Menu.getNextButton().setOnAction(e ->{
-            simulation.next();
-            border.setCenter(simulation.getGrid());
-            //border.setCenter(simulation.getGridTwo());
-        });
-        Menu.getNextButton().setDisable(this.ready.getValue());
-        Menu.getPrevButton().setDisable(this.ready.getValue());
-        Menu.getNewSimButton().setOnAction(e -> {
-            this.simulation = new Simulation((int)Menu.getTargetSlider().getValue(), (int)Menu.getInfectionChanceSlider().getValue());
-            ready.set(false);
-            border.setCenter(this.simulation.getGrid());
-            //border.setCenter(this.simulation.getGridTwo());
-        });
-        Menu.getCloseSimButton().setOnAction(e ->{
-            ready.setValue(true);
-        });
-        Menu.getCloseSimButton().setDisable(this.ready.getValue());
-        border.setLeft(Menu.getBox());
-
-        ready.addListener((observable, oldValue, newValue) -> {
-            System.out.println(newValue);
-            Menu.getNextButton().setDisable(newValue);
-            Menu.getPrevButton().setDisable(newValue);
-            Menu.getCloseSimButton().setDisable(newValue);
-        });
-
-        Scene scene = new Scene(border, 1024, 768);
-        stage.setScene(scene);
-
-        // Add css for styling
-        scene.getStylesheets().clear();
-        scene.getStylesheets().add("http://users.metropolia.fi/~petrasil/style.css");
-
-        stage.show();
+    public static Button getNextButton(){
+        return next;
     }
 
-    public static void main(String[] args){
-        launch(args);
+    public static Button getPrevButton(){
+        return prev;
     }
 
+    public static Button getNewSimButton(){
+        return newSimulation;
+    }
 
+    public static Button getCloseSimButton(){
+        return closeSimulation;
+    }
+
+    public static void createMenu(){
+
+        vbox.setMaxWidth(500.0);
+        //targetCount.setShowTickLabels(true);
+        //targetCount.setShowTickMarks(true);
+        //targetCount.setMajorTickUnit(499);
+        //targetCount.setMinorTickCount(100);
+        //targetCount.setSnapToTicks(true);
+        Label targetCountValue = new Label("Targets: " + Double.toString(targetCount.getValue()));
+        targetCount.valueProperty().addListener((observable, oldValue, newValue) ->{
+            targetCount.setValue(newValue.intValue());
+            targetCountValue.setText("Targets: " + Double.toString(targetCount.getValue()));
+        });
+
+        infectionChance.setShowTickLabels(true);
+        infectionChance.setShowTickMarks(true);
+        infectionChance.setMajorTickUnit(49);
+        infectionChance.setMinorTickCount(1);
+        infectionChance.setSnapToTicks(true);
+        Label infectionChanceValue = new Label("Infection Chance %: " + Double.toString(infectionChance.getValue()));
+        infectionChance.valueProperty().addListener((observable, oldValue, newValue) ->{
+            infectionChance.setValue(newValue.intValue());
+            infectionChanceValue.setText("Infection Chance %: " + Double.toString(infectionChance.getValue()));
+        });
+
+        vbox.getChildren().addAll(targetCountValue, targetCount,infectionChanceValue, infectionChance, next,prev,newSimulation,closeSimulation);
+    }
+
+    public static VBox getBox(){
+        return Menu.vbox;
+    }
+
+    public static Slider getTargetSlider(){
+        return Menu.targetCount;
+    }
+
+    public static Slider getInfectionChanceSlider(){
+        return Menu.infectionChance;
+    }
 }
